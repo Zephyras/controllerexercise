e042124a74e6ee3a85c7c0c42345125e5a0906ac
zephyras <hkw0101@gmail.com>
2023-01-08 12:16:03 +0900

    Initial commit

---
+++ src/main/java/com/codepresso/controllerexercise/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.codepresso.controllerexercise.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+
+    @RequestMapping(value = "/paid")
+    public String getPaidUer() {
+        return "I'm a paid user.";
+    }
+    @RequestMapping(value = "/enterprise")
+    public String getEnterpriseUser() {
+        return "I'm a enterprise user.";
+    }
+}
